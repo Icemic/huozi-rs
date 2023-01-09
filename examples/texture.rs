e48cda3a49bfa22587cbe1a5cc16236057d4f54c
Icemic <bingfeng.web@gmail.com>
2023-01-09 13:52:43 +0800

    feat: better texture generation example

--- examples/texture.rs
+++ examples/texture.rs
@@ -3,8 +3,23 @@ use huozi::charsets::{ASCII, CHS, CJK_SYMBOL};
 fn main() {
     let font_data = std::fs::read("assets/SourceHanSansCN-Normal.otf").unwrap();
     let mut huozi = huozi::Huozi::new(font_data);
-    huozi.preload(ASCII);
-    huozi.preload(CJK_SYMBOL);
-    huozi.preload(CHS);
-    huozi.dump_texture_to("texture_dump.png").unwrap();
+    // for this demo, just load the first 1024 characters, it will completely fill the red channel.
+    huozi.preload(
+        &ASCII
+            .chars()
+            .into_iter()
+            .chain(CJK_SYMBOL.chars().into_iter())
+            .chain(CHS.chars().into_iter())
+            .take(1024)
+            .collect::<String>(),
+    );
+
+    // copy red channel to green and blue channel, then fill alpha channel with 255 for easier viewing
+    let mut img = huozi.texture_image().clone();
+    img.chunks_exact_mut(4).for_each(|chunk| {
+        chunk[1] = chunk[0];
+        chunk[2] = chunk[0];
+        chunk[3] = 255;
+    });
+    img.save("texture_dump.png").unwrap();
 }
