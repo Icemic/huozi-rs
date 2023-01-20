4e1fd9161a3572585fec8dafc61323d697864fbc
Icemic <bingfeng.jw@gmail.com>
2023-01-21 02:49:57 +0800

    fix: texture gen example

--- examples/texture.rs
+++ examples/texture.rs
@@ -2,6 +2,9 @@ use huozi::charsets::{ASCII, CHS, CJK_SYMBOL};
 
 fn main() {
     let font_data = std::fs::read("assets/SourceHanSansCN-Normal.otf").unwrap();
+    // let font_data = std::fs::read("assets/WenQuanYiMicroHei.ttf").unwrap();
+    // let font_data = std::fs::read("assets/PingFang.ttc").unwrap();
+    // let font_data = std::fs::read("assets/msyh.ttf").unwrap();
     let mut huozi = huozi::Huozi::new(font_data);
     // for this demo, just load the first 1024 characters, it will completely fill the red channel.
     huozi.preload(
@@ -17,6 +20,7 @@ fn main() {
     // copy red channel to green and blue channel, then fill alpha channel with 255 for easier viewing
     let mut img = huozi.texture_image().clone();
     img.chunks_exact_mut(4).for_each(|chunk| {
+        chunk[0] = chunk[0];
         chunk[1] = chunk[0];
         chunk[2] = chunk[0];
         chunk[3] = 255;
