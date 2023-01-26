9ba08faca72090b1a79cf614a72c4e4ca90ff3db
Icemic <bingfeng.jw@gmail.com>
2023-01-26 22:34:48 +0800

    feat: move parser tests to mod

--- src/parser.rs
+++ src/parser.rs
@@ -137,165 +137,164 @@ pub fn parse(input: &str) -> ParseResult<Vec<Element>> {
     Ok((input, r))
 }
 
-#[test]
-fn parse_text() {
+#[cfg(test)]
+mod tests {
+    use crate::parser::*;
     use nom::error::convert_error;
-    // let input = "泽材[fillColor=0xff6600]灭[bold]逐[/bold][/fillColor]莫笔[strokeEnable=true]亡[/strokeEnable]鲜，[strokeEnable=true][strokeColor=black][fillColor=red][fontSize=64]如何[/fontSize][fillColor=orange][italic]气[/italic][fillColor=yellow][bold]死[/bold][fillColor=green]你的[fillColor=0xff6600]设[fillColor=blue]计师[fillColor=magenta][fontSize=28]朋[/fontSize]友[/fillColor][/fillColor][/fillColor][/fillColor][/fillColor][/fillColor][/fillColor][/strokeColor][/strokeEnable]";
 
-    let input = r#"ssf[xx="123"]aaa[/xx]"#;
+    #[test]
+    #[ignore]
+    fn parse_text() {
+        // let input = "泽材[fillColor=0xff6600]灭[bold]逐[/bold][/fillColor]莫笔[strokeEnable=true]亡[/strokeEnable]鲜，[strokeEnable=true][strokeColor=black][fillColor=red][fontSize=64]如何[/fontSize][fillColor=orange][italic]气[/italic][fillColor=yellow][bold]死[/bold][fillColor=green]你的[fillColor=0xff6600]设[fillColor=blue]计师[fillColor=magenta][fontSize=28]朋[/fontSize]友[/fillColor][/fillColor][/fillColor][/fillColor][/fillColor][/fillColor][/fillColor][/strokeColor][/strokeEnable]";
 
-    match parse(input) {
-        Ok(r) => println!("{:#?}", r.1),
-        Err(nom::Err::Error(e)) | Err(nom::Err::Failure(e)) => {
-            println!("{}", e);
-            println!("{}", convert_error(input, e));
-        }
-        Err(nom::Err::Incomplete(n)) => {
-            println!("{:?}", n);
+        let input = r#"ssf[xx="123"]aaa[/xx]"#;
+
+        match parse(input) {
+            Ok(r) => println!("{:#?}", r.1),
+            Err(nom::Err::Error(e)) | Err(nom::Err::Failure(e)) => {
+                println!("{}", e);
+                println!("{}", convert_error(input, e));
+            }
+            Err(nom::Err::Incomplete(n)) => {
+                println!("{:?}", n);
+            }
         }
     }
-}
 
-#[test]
-fn test_plain_text() {
-    assert_eq!(
-        parse(" some text ").unwrap().1,
-        vec![Element::Text(" some text ")]
-    );
-}
+    #[test]
+    fn plain_text() {
+        assert_eq!(
+            parse(" some text ").unwrap().1,
+            vec![Element::Text(" some text ")]
+        );
+    }
 
-#[test]
-fn test_plain_text_escaped() {
-    assert_eq!(
-        parse(r" some \n \[text ").unwrap().1,
-        vec![Element::Text(r" some \n \[text ")]
-    );
-}
+    #[test]
+    fn plain_text_escaped() {
+        assert_eq!(
+            parse(r" some \n \[text ").unwrap().1,
+            vec![Element::Text(r" some \n \[text ")]
+        );
+    }
 
-#[test]
-fn test_single_block_without_value() {
-    assert_eq!(
-        parse(r"[foo]text[/foo]").unwrap().1,
-        vec![Element::Block(Block {
-            inner: vec![Element::Text("text")],
-            tag: "foo",
-            value: None
-        })]
-    );
-}
+    #[test]
+    fn single_block_without_value() {
+        assert_eq!(
+            parse(r"[foo]text[/foo]").unwrap().1,
+            vec![Element::Block(Block {
+                inner: vec![Element::Text("text")],
+                tag: "foo",
+                value: None
+            })]
+        );
+    }
 
-#[test]
-fn test_single_block_with_value() {
-    assert_eq!(
-        parse(r"[foo=bar]text[/foo]").unwrap().1,
-        vec![Element::Block(Block {
-            inner: vec![Element::Text("text")],
-            tag: "foo",
-            value: Some("bar")
-        })]
-    );
-}
-
-#[test]
-fn test_single_block_with_value_quoted() {
-    assert_eq!(
-        parse(r#"[foo="bar "]text[/foo]"#).unwrap().1,
-        vec![Element::Block(Block {
-            inner: vec![Element::Text("text")],
-            tag: "foo",
-            value: Some("bar ")
-        })]
-    );
-}
-
-#[test]
-fn test_single_block_multiline() {
-    assert_eq!(
-        parse(
-            r"[foo=bar]
-text
-  
-[/foo]"
-        )
-        .unwrap()
-        .1,
-        vec![Element::Block(Block {
-            inner: vec![Element::Text("\ntext\n  \n")],
-            tag: "foo",
-            value: Some("bar")
-        })]
-    );
-}
-
-#[test]
-fn test_mixed_text_and_block() {
-    assert_eq!(
-        parse(r" some text [foo=bar]text[/foo]").unwrap().1,
-        vec![
-            Element::Text(" some text "),
-            Element::Block(Block {
+    #[test]
+    fn single_block_with_value() {
+        assert_eq!(
+            parse(r"[foo=bar]text[/foo]").unwrap().1,
+            vec![Element::Block(Block {
                 inner: vec![Element::Text("text")],
                 tag: "foo",
                 value: Some("bar")
-            })
-        ]
-    );
-}
+            })]
+        );
+    }
 
-#[test]
-fn test_nested_blocks() {
-    assert_eq!(
-        parse(r"[foo=bar][xx=123][/xx][/foo]").unwrap().1,
-        vec![Element::Block(Block {
-            inner: vec![Element::Block(Block {
-                inner: vec![],
-                tag: "xx",
-                value: Some("123")
-            })],
-            tag: "foo",
-            value: Some("bar")
-        })]
-    );
-}
+    #[test]
+    fn single_block_with_value_quoted() {
+        assert_eq!(
+            parse(r#"[foo="bar "]text[/foo]"#).unwrap().1,
+            vec![Element::Block(Block {
+                inner: vec![Element::Text("text")],
+                tag: "foo",
+                value: Some("bar ")
+            })]
+        );
+    }
 
-#[test]
-fn test_complex_elements() {
-    assert_eq!(
-        parse(r"a\n[foo=bar]q[xx=123][/xx]x[/foo][yy][/yy]")
-            .unwrap()
-            .1,
-        vec![
-            Element::Text("a\\n"),
-            Element::Block(Block {
-                inner: vec![
-                    Element::Text("q"),
-                    Element::Block(Block {
-                        inner: vec![],
-                        tag: "xx",
-                        value: Some("123")
-                    }),
-                    Element::Text("x")
-                ],
+    #[test]
+    fn single_block_multiline() {
+        assert_eq!(
+            parse("[foo=bar]\ntext\n  \n[/foo]").unwrap().1,
+            vec![Element::Block(Block {
+                inner: vec![Element::Text("\ntext\n  \n")],
                 tag: "foo",
                 value: Some("bar")
-            }),
-            Element::Block(Block {
-                inner: vec![],
-                tag: "yy",
-                value: None
-            })
-        ]
-    );
-}
+            })]
+        );
+    }
 
-#[test]
-fn test_tagpair_with_spaces() {
-    assert_eq!(
-        parse(r#"[ foo = "bar " ]text[/ foo  ]"#).unwrap().1,
-        vec![Element::Block(Block {
-            inner: vec![Element::Text("text")],
-            tag: "foo",
-            value: Some("bar ")
-        })]
-    );
+    #[test]
+    fn mixed_text_and_block() {
+        assert_eq!(
+            parse(r" some text [foo=bar]text[/foo]").unwrap().1,
+            vec![
+                Element::Text(" some text "),
+                Element::Block(Block {
+                    inner: vec![Element::Text("text")],
+                    tag: "foo",
+                    value: Some("bar")
+                })
+            ]
+        );
+    }
+
+    #[test]
+    fn nested_blocks() {
+        assert_eq!(
+            parse(r"[foo=bar][xx=123][/xx][/foo]").unwrap().1,
+            vec![Element::Block(Block {
+                inner: vec![Element::Block(Block {
+                    inner: vec![],
+                    tag: "xx",
+                    value: Some("123")
+                })],
+                tag: "foo",
+                value: Some("bar")
+            })]
+        );
+    }
+
+    #[test]
+    fn complex_elements() {
+        assert_eq!(
+            parse(r"a\n[foo=bar]q[xx=123][/xx]x[/foo][yy][/yy]")
+                .unwrap()
+                .1,
+            vec![
+                Element::Text("a\\n"),
+                Element::Block(Block {
+                    inner: vec![
+                        Element::Text("q"),
+                        Element::Block(Block {
+                            inner: vec![],
+                            tag: "xx",
+                            value: Some("123")
+                        }),
+                        Element::Text("x")
+                    ],
+                    tag: "foo",
+                    value: Some("bar")
+                }),
+                Element::Block(Block {
+                    inner: vec![],
+                    tag: "yy",
+                    value: None
+                })
+            ]
+        );
+    }
+
+    #[test]
+    fn tagpair_with_spaces() {
+        assert_eq!(
+            parse(r#"[ foo = "bar " ]text[/ foo  ]"#).unwrap().1,
+            vec![Element::Block(Block {
+                inner: vec![Element::Text("text")],
+                tag: "foo",
+                value: Some("bar ")
+            })]
+        );
+    }
 }
