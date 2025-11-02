803341a534f4b6b38f9d32265aa75289ff6cc756
Icemic <bingfeng.web@gmail.com>
2025-11-02 19:43:19 +0800

    feat: add PartialEq and Eq traits to LayoutDirection enum

--- src/layout/layout_style.rs
+++ src/layout/layout_style.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum LayoutDirection {
     #[default]
