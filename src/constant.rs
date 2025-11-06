6c5499ae1bc75eb54ba1237476ed01c137422eb1
Icemic <bingfeng.web@gmail.com>
2025-11-06 21:54:06 +0800

    fix: update RADIUS constant from 16 to 24 for better stroke and shadow

--- src/constant.rs
+++ src/constant.rs
@@ -1,7 +1,7 @@
 pub const GRID_SIZE: f64 = 128.;
 pub const FONT_SIZE: f64 = 96.;
 pub const BUFFER: f64 = 16.;
-pub const RADIUS: f64 = 16.;
+pub const RADIUS: f64 = 24.;
 pub const CUTOFF: f64 = 0.25;
 pub const TEXTURE_SIZE: u32 = 2048;
 // 112 is just a magic number, may should be replaced by more reasonable algorithm
